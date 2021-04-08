0203b301deb71f57927334c71146b18ac668c141
jld3103 <jld3103yt@gmail.com>
2021-04-08 15:27:53 +0200

    Make builds smaller

--- build-debug.sh
+++ build-debug.sh
@@ -4,5 +4,5 @@ RECONFIGURE=""
 if [ -d debug-build ]; then
   RECONFIGURE="--reconfigure"
 fi
-meson --prefix="$(readlink -f ../debug-build)" -Dgallium-drivers=radeonsi,swrast --buildtype=debug -Db_lto=false -Dn_debug=false debug-build $RECONFIGURE
+meson --prefix="$(readlink -f ../debug-build)" -Dgallium-drivers=radeonsi -Dvulkan-drivers= -Ddri-drivers= -Degl=disabled -Dgbm=disabled --buildtype=debug -Db_lto=false -Dn_debug=false debug-build $RECONFIGURE
 ninja -C debug-build install
