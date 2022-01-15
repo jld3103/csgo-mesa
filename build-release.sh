6c644a4b925f4c056bfedbf7c0572a650138c88c
jld3103 <jld3103yt@gmail.com>
2022-01-15 16:12:50 +0100

    Update mesa and fix meson arguments

--- build-release.sh
+++ build-release.sh
@@ -5,5 +5,5 @@ RECONFIGURE=""
 if [ -d release-build ]; then
   RECONFIGURE="--reconfigure"
 fi
-meson --prefix="$(readlink -f ../release-build)" -Dgallium-drivers=radeonsi -Dvulkan-drivers= -Ddri-drivers= -Degl=disabled -Dgbm=disabled --buildtype=release -Db_lto=true -Dn_debug=true release-build $RECONFIGURE
+meson --prefix="$(readlink -f ../release-build)" -Dgallium-drivers=radeonsi -Dvulkan-drivers= -Ddri-drivers= -Degl=disabled -Dgbm=disabled --buildtype=release -Db_lto=true -Db_ndebug=true release-build $RECONFIGURE
 ninja -C release-build install
